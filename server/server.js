61fe9ef0e6b016ba28ce7b5c392e3dd91eb57de1
Fabulosa <fabiscg79@gmail.com>
2022-03-17 12:30:16 -0400

    deleting node_modules, starting to eject from cra

--- server/server.js
+++ server/server.js
@@ -12,10 +12,12 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
+  })
 }
 
 app.use(routes);
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`🌍 Now listening on 0.0.0.0:27017:${PORT}`));
 });
